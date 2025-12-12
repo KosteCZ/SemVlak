a19b0ec4ad18fc80c54fab73b6e3083417e7c909
KosteCZ <KosteCZ@users.noreply.github.com>
2025-12-12 22:32:38 +0100

    Added railroad crossing lights blinking

--- src/main/java/cz/koscak/jan/Main.java
+++ src/main/java/cz/koscak/jan/Main.java
@@ -5,7 +5,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main extends JPanel implements ActionListener, KeyListener {
 
@@ -98,12 +97,12 @@ public class Main extends JPanel implements ActionListener, KeyListener {
         listOfRailroadCrossings = new ArrayList<>();
 
         final RailroadCrossing railroadCrossing1 = new RailroadCrossing(
-                100, 550, 0, 1, RailroadCrossing.Light.RED,
+                100, 550, 0, 1, RailroadCrossing.State.RED,
                 50, 250, 600, 625);
         listOfRailroadCrossings.add(railroadCrossing1);
 
         final RailroadCrossing railroadCrossing2 = new RailroadCrossing(
-                125, 650, 0, -1, RailroadCrossing.Light.RED,
+                125, 650, 0, -1, RailroadCrossing.State.RED,
                 50, 250, 600, 625);
         listOfRailroadCrossings.add(railroadCrossing2);
     }
@@ -204,13 +203,17 @@ public class Main extends JPanel implements ActionListener, KeyListener {
         g.drawImage(Images.rotate(getImage(Image.TUNNEL_VERTICAL_UP_ENTRY_2), 90.0), 0, 600, this);
         g.drawImage(Images.rotate(getImage(Image.TUNNEL_VERTICAL_UP_ENTRY_2), 270.0), 400, 600, this);
 
-        g.drawImage(Images.rotate(getImage(Image.RAILROAD_CROSSING), 0.0), 50, 550, this);
+        RailroadCrossing railroadCrossing1 = listOfRailroadCrossings.get(0);
+        BufferedImage imageOfRailroadCrossing1 = getImage(railroadCrossing1.getImage());
+        g.drawImage(Images.rotate(imageOfRailroadCrossing1, 0.0), 50, 550, this);
         BufferedImage imageOfBar1 = getImage(Image.RAILROAD_CROSSING_BAR);
         BufferedImage imageOfBarResized1 = Images.resize(imageOfBar1,
-                imageOfBar1.getWidth() - listOfRailroadCrossings.get(0).getBarState(), imageOfBar1.getHeight());
-        g.drawImage(Images.rotate(imageOfBarResized1, 0.0), 50 + (listOfRailroadCrossings.get(0).getBarState() / 3), 550, this);
+                imageOfBar1.getWidth() - railroadCrossing1.getBarState(), imageOfBar1.getHeight());
+        g.drawImage(Images.rotate(imageOfBarResized1, 0.0), 50 + (railroadCrossing1.getBarState() / 3), 550, this);
 
-        g.drawImage(Images.rotate(getImage(Image.RAILROAD_CROSSING), 180.0), 150, 650, this);
+        RailroadCrossing railroadCrossing2 = listOfRailroadCrossings.get(1);
+        BufferedImage imageOfRailroadCrossing2 = getImage(railroadCrossing2.getImage());
+        g.drawImage(Images.rotate(imageOfRailroadCrossing2, 180.0), 150, 650, this);
         BufferedImage imageOfBar2 = getImage(Image.RAILROAD_CROSSING_BAR);
         BufferedImage imageOfBarResized2 = Images.resize(imageOfBar2,
                 imageOfBar2.getWidth() - listOfRailroadCrossings.get(1).getBarState(), imageOfBar2.getHeight());
@@ -281,6 +284,7 @@ public class Main extends JPanel implements ActionListener, KeyListener {
         if (!gamePaused && !gameWon && !gameOver) {  // Only update game logic when not paused or won/over
             //ToDo
             for(RailroadCrossing railroadCrossing : listOfRailroadCrossings) {
+                railroadCrossing.time();
                 railroadCrossing.checkState(listOfTrains);
             }
             for(Car car : listOfCars) {
--- src/main/java/cz/koscak/jan/RailroadCrossing.java
+++ src/main/java/cz/koscak/jan/RailroadCrossing.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class RailroadCrossing {
 
     private int stopX, stopY, vx, vy, checkX1, checkX2, checkY1, checkY2;
-    private RailroadCrossing.Light state;
+    private State state;
+    private Light light;
     private int barState = 0;
+    private int timeToNextLight = 7;
 
-    public RailroadCrossing(int stopX, int stopY, int vx, int vy, Light state,
+    public RailroadCrossing(int stopX, int stopY, int vx, int vy, State state,
                             int checkX1, int checkX2, int checkY1, int checkY2) {
         this.stopX = stopX;
         this.stopY = stopY;
@@ -19,10 +21,20 @@ public class RailroadCrossing {
         this.checkX2 = checkX2;
         this.checkY1 = checkY1;
         this.checkY2 = checkY2;
+        if (state == State.RED) {
+            light = Light.RED_LEFT;
+        } else {
+            light = Light.WHITE;
+        }
+        timeToNextLight = 7;
+    }
+
+    public enum State {
+        RED, WHITE
     }
 
     public enum Light {
-        RED, WHITE
+        RED_LEFT, RED_RIGHT, WHITE, NONE
     }
 
     public boolean stop(Car car) {
@@ -31,27 +43,34 @@ public class RailroadCrossing {
                     || (vx < 0 && car.getVX() < 0)
                     || (vy > 0 && car.getVY() > 0)
                     || (vy < 0 && car.getVY() < 0)) {
-                return state.equals(Light.RED);
+                return state.equals(State.RED);
             }
         }
         return false;
     }
 
     public void checkState(List<Train> listOfTrains) {
-        Light newLightState = Light.WHITE;
+        State newState = State.WHITE;
         for (Train train : listOfTrains) {
             if (train.getX() >= checkX1
                     && train.getX() <= checkX2
                     && train.getY() >= checkY1
                     && train.getY() <= checkY2) {
-                newLightState = Light.RED;
+                newState = State.RED;
             }
         }
-        state = newLightState;
-        if (newLightState == Light.RED && barState > 0) {
+        if (state != newState) {
+            state = newState;
+            if (newState == State.RED) {
+                light = Light.RED_LEFT;
+            } else {
+                light = Light.WHITE;
+            }
+        }
+        if (newState == State.RED && barState > 0) {
             barState = barState - 5;
         }
-        if (newLightState == Light.WHITE && barState < 60) {
+        if (newState == State.WHITE && barState < 60) {
             barState = barState + 5;
         }
     }
@@ -59,4 +78,37 @@ public class RailroadCrossing {
     public int getBarState() {
         return barState;
     }
+
+    public void time() {
+        if (timeToNextLight <= 0) {
+            timeToNextLight = 7;
+            Light nextLight = Light.NONE;
+            if (light == Light.WHITE) {
+                nextLight = Light.NONE;
+            } else if (light == Light.NONE) {
+                nextLight = Light.WHITE;
+            } else if (light == Light.RED_LEFT) {
+                nextLight = Light.RED_RIGHT;
+            } else if (light == Light.RED_RIGHT) {
+                nextLight = Light.RED_LEFT;
+            }
+            light = nextLight;
+        }
+        timeToNextLight--;
+    }
+
+    public Light getLight() {
+        return light;
+    }
+
+    public Image getImage() {
+        return switch (light) {
+            case NONE -> Image.RAILROAD_CROSSING_NO_LIGHT;
+            case WHITE -> Image.RAILROAD_CROSSING_WHITE;
+            case RED_LEFT -> Image.RAILROAD_CROSSING_RED_LEFT;
+            case RED_RIGHT -> Image.RAILROAD_CROSSING_RED_RIGHT;
+            default -> Image.RAILROAD_CROSSING_NO_LIGHT;
+        };
+    }
+
 }
